245c7684cfacf284ea44a3e126227020f854778e
Mateusz Kłosiński <mkklosin@gmail.com>
2018-11-07 17:40:35 +0100

    notification create via CLI

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { LogoComponent } from './logo/logo.component';
 import { MaterialModule } from './material/material.module';
 import { FormComponent } from './form/form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NotificationComponent } from './notification/component/notification.component';
 
 @NgModule({
-  declarations: [LogoComponent, FormComponent],
+  declarations: [LogoComponent, FormComponent, NotificationComponent],
   imports: [CommonModule, MaterialModule, ReactiveFormsModule],
   exports: [LogoComponent, MaterialModule, FormComponent, ReactiveFormsModule]
 })
