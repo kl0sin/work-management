e036b40b98c3d1e0a478ad6e780bd65680520919
Mateusz Kłosiński <mkklosin@gmail.com>
2018-11-11 15:18:02 +0100

    routerLink's added to navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavigationComponent, FooterComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, RouterModule],
   exports: [NavigationComponent, FooterComponent]
 })
 export class CoreModule {}
