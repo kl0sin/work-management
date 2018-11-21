e967967a0a019e4f4eaf94b96258e124e0eaaff7
Mateusz Kłosiński <mkklosin@gmail.com>
2018-11-21 23:09:32 +0100

    create user, checking user email in DB

--- server/routes/api.js
+++ server/routes/api.js
@@ -42,15 +42,26 @@ router.get('/', (req, res) => {
 router.post('/register', (req, res) => {
   let userData = req.body;
   let user = new User(userData);
-  user.save((error, user) => {
-    if (error) {
-      console.error(error);
+
+  User.findOne({ email: userData.email }, (err, user) => {
+    if (err) {
+      console.log(err);
     } else {
-      let payload = { subject: user._id };
-      let token = jwt.sign(payload, 'someSecretKey');
-      res.status(200).send({ token });
+      if (user) {
+        res.status(401).send('Email already exists!');
+      } else {
+        user.save((error, user) => {
+          if (error) {
+            console.error(error);
+          } else {
+            let payload = { subject: user._id };
+            let token = jwt.sign(payload, 'someSecretKey');
+            res.status(200).send({ token });
+          }
+        });
+      }
     }
-  });
+  })
 });
 
 router.post('/login', (req, res) => {
